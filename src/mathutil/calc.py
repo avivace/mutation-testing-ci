64895bb31d60c3c343e204b167151a44427664d2
Antonio Vivace <antonio.vivace@datadoghq.com>
2026-09-23 13:30:05 +0200

    Document math utility functions

--- src/mathutil/calc.py
+++ src/mathutil/calc.py
@@ -1,8 +1,10 @@
 def add(a, b):
+    """Return the sum of a and b."""
     return a + b
 
 
 def clamp(value, low, high):
+    """Constrain value to the inclusive range from low to high."""
     if value < low:
         return low
     if value > high:
